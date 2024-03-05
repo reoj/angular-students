5c3b7c1817a24671ae88e576fa963725205cb9b2
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2024-03-05 07:40:07 -0600

    Revert "Adds the ReactiveForms Module"
    This reverts commit 82904e56ff66cec804b7686bd07c8b8f01e943eb.

--- src/Components/shared/shared/shared.module.ts
+++ src/Components/shared/shared/shared.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EntityViewerComponent } from './entity-viewer/entity-viewer.component';
 import { AddObjectFormComponent } from './add-object-form/add-object-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [EntityViewerComponent, AddObjectFormComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule],
   exports: [EntityViewerComponent, AddObjectFormComponent],
 })
 export class SharedModule {}
