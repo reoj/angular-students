df833867c2a9c3697d0ba00cdad729e007d7053d
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2024-03-21 07:24:28 -0600

    Adds Angular Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,19 @@ import { SharedModule } from 'src/Components/shared/shared/shared.module';
 import { CoursesComponent } from 'src/Components/courses/courses.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProjectsComponent } from 'src/Components/projects/projects.component';
+import { RouterModule } from '@angular/router';
+import { Route } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, StudentsComponent, CoursesComponent, ProjectsComponent],
-  imports: [BrowserModule, SharedModule, ReactiveFormsModule, FormsModule],
+  imports: [BrowserModule, SharedModule, ReactiveFormsModule, FormsModule, RouterModule.forRoot(
+    [
+      {path: 'students', component: StudentsComponent},
+      {path: 'courses', component: CoursesComponent},
+      {path: 'projects', component: ProjectsComponent},
+    ]
+  
+  )],
   providers: [],
   bootstrap: [AppComponent],
 })
